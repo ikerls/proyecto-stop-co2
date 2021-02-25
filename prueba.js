d7f611fa6e4156aafb9d856e2b2e90572d8525b9
Armando-Rondon <71701945+Armando-Rondon@users.noreply.github.com>
2021-02-25 11:05:55 +0100

    Add files via upload
    footer

---
+++ prueba.js
@@ -0,0 +1,7 @@
+const boton = document.querySelector("#boton");
+const menu = document.querySelector("#menu");
+
+boton.addEventListener("click", () => {
+  console.log("me diste click");
+  menu.classList.toggle("hidden");
+});
