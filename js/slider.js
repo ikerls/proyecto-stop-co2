0e1e16e322d43f7ec31d8927792a67b8624994a3
Ikerls <ikerlarrea150@gmail.com>
2021-04-17 10:10:04 +0200

    añadido un slider para seccion de la galeria

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+var splide;
+document.addEventListener("DOMContentLoaded", function () {
+  splide = new Splide("#image-slider").mount();
+});
+
+/*splide.on("click", function () {
+  alert("holsz");
+});
+*/
